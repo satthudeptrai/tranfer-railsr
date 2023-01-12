0f8a38c2a5f40840752cf33659ea59faf35947d0
Đỗ Ngọc Ánh <anhdn1@tinhvan.com>
2023-01-12 15:13:24 +0700

    2 commit

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,30 +1,34 @@
 const axios = require('axios');
 const TransactionsModel = require('../models/transactionsModel');
 class TransactionsController {
-  tranferData = async () => {
+  tranferData = async (firstDate, lastDate) => {
     try {
+      console.log(`tranfer transaction ${firstDate} start`)
       const items_per_page = 100;
-      let page_card = 0;
+      let page = 0;
       for( ; ; ) {
         const { data: arrayData } = await axios({
           method: 'get',
           url: '/customer/transactions',
           params: {
             items_per_page: items_per_page,
-            offset: page_card * items_per_page,
+            offset: page * items_per_page,
             // from_date: process.argv[3] || "2019-01-01"
+            from_date: `${firstDate}`,
+            to_date: `${lastDate}`,
           }
         });
         TransactionsModel.create(arrayData);
         if (arrayData.length <= 0) {
-          console.log("tranfer transaction done")
+          console.log(`tranfer transaction ${firstDate} done`)
+          console.log(`page ${page}`)
           break;
         }
-        page_card ++;
+        page ++;
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 }
 module.exports = new TransactionsController;
--- src/index.js
+++ src/index.js
@@ -5,19 +5,65 @@ const BeneficiariesController = require('./controllers/beneficiaries');
 const CardController = require('./controllers/cards');
 const LedgerController = require('./controllers/ledgers');
 const TransactionsController = require('./controllers/transactions');
+
 const traferData = async () => {
+  const d = new Date();
+  const thisMonth = d.getMonth();
+  const thisYear = d.getFullYear();
+  const listDate = [];
+  for(let y = 2019; y <= thisYear; y++) {
+    if(y === thisYear) {
+      for(let m = 0; m <= thisMonth; m++) {
+        const firstDate = new Date(y, m, 1);
+        const lastDate = new Date(y, m + 1, 0);
+        listDate.push({
+          firstDate: `${firstDate.getFullYear()}-${firstDate.getMonth() + 1}-${firstDate.getDate()}T00:00:00.000Z`,
+          lastDate: `${lastDate.getFullYear()}-${lastDate.getMonth() + 1}-10T23:59:59.999Z`
+        });
+        listDate.push({
+          firstDate: `${firstDate.getFullYear()}-${firstDate.getMonth() + 1}-10T00:00:00.000Z`,
+          lastDate: `${lastDate.getFullYear()}-${lastDate.getMonth() + 1}-20T23:59:59.999Z`
+        });
+        listDate.push({
+          firstDate: `${firstDate.getFullYear()}-${firstDate.getMonth() + 1}-20T00:00:00.000Z`,
+          lastDate: `${lastDate.getFullYear()}-${lastDate.getMonth() + 1}-${lastDate.getDate()}T23:59:59.999Z`
+        });
+      }
+    } else {
+      for(let m = 0; m <= 11; m++) {
+        const firstDate = new Date(y, m, 1);
+        const lastDate = new Date(y, m + 1, 0);
+        listDate.push({
+          firstDate: `${firstDate.getFullYear()}-${firstDate.getMonth() + 1}-${firstDate.getDate()}T00:00:00.000Z`,
+          lastDate: `${lastDate.getFullYear()}-${lastDate.getMonth() + 1}-10T23:59:59.999Z`
+        });
+        listDate.push({
+          firstDate: `${firstDate.getFullYear()}-${firstDate.getMonth() + 1}-10T00:00:00.000Z`,
+          lastDate: `${lastDate.getFullYear()}-${lastDate.getMonth() + 1}-20T23:59:59.999Z`
+        });
+        listDate.push({
+          firstDate: `${firstDate.getFullYear()}-${firstDate.getMonth() + 1}-20T00:00:00.000Z`,
+          lastDate: `${lastDate.getFullYear()}-${lastDate.getMonth() + 1}-${lastDate.getDate()}T23:59:59.999Z`
+        });
+      }
+    }
+  }
   await setup();
   await connect();
   const tranferEnduser = EndusersController.tranferData();
   const tranferBeneficiarie = BeneficiariesController.tranferData();
   const tranferCard = CardController.tranferData();
   const tranferLedger = LedgerController.tranferData();
-  const tranferTransaction = TransactionsController.tranferData();
+  const listTransaction = listDate.map(item => {
+    return TransactionsController.tranferData(item.firstDate, item.lastDate);
+  });
+  const listTranferTransaction = Promise.all(listTransaction);
   await tranferEnduser;
   await tranferBeneficiarie;
   await tranferCard;
   await tranferLedger;
-  await tranferTransaction;
+  await listTranferTransaction;
   await disconnect();
 };
+
 traferData();
